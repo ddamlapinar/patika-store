0133e2f10c4822ce38792455b5890394e75264d0
Didem Gülşen Damlapınar <35114789+ddamlapinar@users.noreply.github.com>
2021-09-12 01:19:46 +0300

    feat: :sparkles: Add search functionality

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, FlatList, StyleSheet, TextInput} from 'react-native';
 import products_data from './products_data';
 import ProductCard from './components/ProductCard';
 
 function App() {
+  const [list, setList] = useState(products_data);
+  const handleSearch = text => {
+    const filteredList = products_data.filter(item => {
+      const searchedText = text.toLowerCase();
+      const product = item.title.toLowerCase();
+      return product.includes(searchedText);
+    });
+    setList(filteredList);
+  };
   const renderProducts = ({item}) => <ProductCard products={item} />;
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>
-        Patika Store
-      </Text>
+      <Text style={styles.title}>Patika Store</Text>
 
-      <TextInput style={styles.input}>Search</TextInput>
+      <TextInput
+        onChangeText={handleSearch}
+        style={styles.input}
+        placeholder="Search"
+      />
       <FlatList
-        data={products_data}
+        data={list}
         renderItem={renderProducts}
         numColumns={2}
         keyExtractor={item => item.id}
